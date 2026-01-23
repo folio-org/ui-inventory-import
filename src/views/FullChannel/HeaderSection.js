7186255b8e41e93a745e55fbfa0636ebc63feaf7
Mike Taylor <mike@indexdata.com>
2026-01-23 19:17:35 +0000

    Add in missing fields

--- src/views/FullChannel/HeaderSection.js
+++ src/views/FullChannel/HeaderSection.js
@@ -9,13 +9,20 @@ const HeaderSection = ({ rec }) => (
     id="harvestable-section-general"
     label={<FormattedMessage id="ui-inventory-import.channels.heading.status" />}
   >
-    <RCKV rec={rec} tag="type" i18nTag="jobClass" xs={4} />
+    <RCKV rec={rec} tag="type" xs={4} />
     <Row>
-      <CKV rec={rec} tag="id" xs={2} />
+      <CKV rec={rec} tag="id" xs={4} />
+      <CKV rec={rec} tag="tag" xs={2} />
       <CKV rec={rec} tag="name" xs={6} />
     </Row>
     <Row>
       <CKV rec={rec} tag="enabled" xs={4} />
+    </Row>
+    <Row>
+      <CKV rec={rec} tag="isCommissioned" xs={4} />
+    </Row>
+    <Row>
+      <CKV rec={rec} tag="listening" xs={4} />
     </Row>
     {/* XXX we want to look this up by name */}
     <RCKV rec={rec} tag="transformationId" i18nTag="transformationPipeline" />
