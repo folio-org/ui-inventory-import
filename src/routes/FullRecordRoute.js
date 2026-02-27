12baeb9249d7e3cab7ad460a7a90c16c492e049f
Mike Taylor <mike@indexdata.com>
2026-02-27 16:22:39 +0000

    Use new WSAPI to fetch a single failed record
    Fixes #13
    
    Fixes UIINIMP-20.

--- src/routes/FullRecordRoute.js
+++ src/routes/FullRecordRoute.js
@@ -5,28 +5,15 @@ import FullRecord from '../views/FullRecord';
 import packageInfo from '../../package';
 
 
-// There is no WASAPI for GET inventory-import/failed-records/:{recId}
-// so we can't simply fetch the record we want. Instead we rely on
-// this page only every being invoked as part of the
-// list-of-failed-records page populated by RecordsRoute.js, and
-// assume that its "records" resource is alreaduy populated. We use
-// match.params.recId to pull out the selected record, and pass that
-// down to the view.
-
-const FullRecordRoute = ({ resources, mutator, match }) => {
-  const handleClose = () => {
-    mutator.query.update({ _path: `${packageInfo.stripes.route}/records` });
-  };
-
-  const records = resources.records.records || [];
-  const matching = records.filter(x => x.id === match.params.recId);
-
+const FullRecordRoute = ({ resources, mutator }) => {
   return (
     <FullRecord
       data={{
-        record: matching[0],
+        record: resources.failedRecord.records[0],
       }}
-      handlers={{ onClose: handleClose }}
+      handlers={{
+        onClose: () => mutator.query.update({ _path: `${packageInfo.stripes.route}/records` })
+      }}
     />
   );
 };
@@ -34,38 +21,23 @@ const FullRecordRoute = ({ resources, mutator, match }) => {
 
 FullRecordRoute.manifest = {
   query: {},
-  records: {
-    // No need to specify anything to do for this resource,
-    // all the work happens when RecordsRoute is loaded.
+  failedRecord: {
+    type: 'okapi',
+    path: 'inventory-import/failed-records/:{recId}',
   },
 };
 
 FullRecordRoute.propTypes = {
   resources: PropTypes.shape({
-    records: PropTypes.shape({
-      failed: PropTypes.oneOfType([
-        PropTypes.bool,
-        PropTypes.shape({
-          message: PropTypes.string.isRequired,
-        })
-      ]).isRequired,
-      hasLoaded: PropTypes.bool.isRequired,
+    failedRecord: PropTypes.shape({
       records: PropTypes.arrayOf(
         PropTypes.object.isRequired,
       ),
-      other: PropTypes.shape({
-        totalRecords: PropTypes.number.isRequired,
-      }),
     }),
   }).isRequired,
   mutator: PropTypes.shape({
     query: PropTypes.shape({
       update: PropTypes.func.isRequired,
-    }).isRequired,
-  }).isRequired,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      recId: PropTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
 };
