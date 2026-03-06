615f0aad3a74e0cc7b169a0d97fa0e8ab10d5a7e
Mike Taylor <mike@indexdata.com>
2026-03-06 17:19:37 +0000

    Working test for JobsRoute

--- src/routes/JobsRoute.test.js
+++ src/routes/JobsRoute.test.js
@@ -1,41 +1,54 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { cleanup, render, screen, fireEvent } from '@folio/jest-config-stripes/testing-library/react';
+import { render, screen } from '@folio/jest-config-stripes/testing-library/react';
 import { Paneset } from '@folio/stripes/components';
 import withIntlConfiguration from '../../test/jest/util/withIntlConfiguration';
 import JobsRoute from './JobsRoute';
 import jobsData from '../../test/jest/data/jobs';
 
 
-const renderJobsRoute = () => {
+const renderJobsRoute = (jobsResource) => {
   return render(withIntlConfiguration(
     <BrowserRouter>
       <Paneset>
         <JobsRoute
-          hasLoaded
-          data={{ jobs: jobsData }}
-          query={{}}
-          updateQuery={() => undefined}
-          pageAmount={100}
-          onNeedMoreData={() => undefined}
-        />
+          stripes={{
+            logger: {
+            },
+          }}
+          resources={{
+            query: {},
+            jobs: jobsResource,
+          }}
+          mutator={{
+            query: {
+              update: () => undefined,
+            }
+          }}
+          match={{
+            params: {
+              recId: undefined,
+            }
+          }}
+        >
+          <div />
+        </JobsRoute>
       </Paneset>
     </BrowserRouter>
   ));
 };
 
 
-describe('Jobs page', () => {
-  let node;
-
-  beforeEach(() => {
-    node = renderJobsRoute();
-    // screen.debug(undefined, 100000);
-  });
-
-  afterEach(cleanup);
-
-  it('should be rendered', async () => {
+describe('Jobs route', () => {
+  it('should be rendered with data', async () => {
+    const node = renderJobsRoute({
+      failed: false,
+      hasLoaded: true,
+      records: jobsData,
+      other: {
+        totalRecords: jobsData.length,
+      },
+    });
     const { container } = node;
     const content = container.querySelector('[data-test-jobs-paneset]');
     expect(container).toBeVisible();
@@ -44,15 +57,21 @@ describe('Jobs page', () => {
     // Data rendered in the MCL
     const nameCell = screen.getByText("Felix's test channel #1").closest('[role="gridcell"]');
     expect(nameCell?.nextElementSibling).toHaveTextContent(/INTERRUPTED/);
+  });
 
-    // Change selected index
-    const selectElement = document.getElementById('input-jobs-search-qindex');
-    fireEvent.change(selectElement, { target: { value: 'channelName' } });
-    expect(selectElement.value).toBe('channelName');
+  it('should be rendered with an error', async () => {
+    const node = renderJobsRoute({
+      failed: { message: 'Error provoked by test harness' },
+      hasLoaded: false,
+      records: [],
+    });
+    const { container } = node;
+    const content = container.querySelector('[data-test-jobs-paneset]');
+    expect(container).toBeVisible();
+    expect(content).toBeVisible();
 
-    // Reset all search-form elements
-    const clearElement = document.getElementById('clickable-reset-all');
-    fireEvent.click(clearElement);
-    // Is there a way to check the outcome?
+    // Error message rendered instead of MCL
+    expect(screen.getByText('Error provoked by test harness')).toBeVisible();
+    expect(screen.queryByText("Felix's test channel #1")).not.toBeInTheDocument();
   });
 });
