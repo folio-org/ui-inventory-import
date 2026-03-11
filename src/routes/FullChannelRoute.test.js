54a2b7741ada7565da3b898ca8516253eff901e8
Mike Taylor <mike@indexdata.com>
2026-03-11 14:38:44 +0000

    Add test for full-channel route

--- src/routes/FullChannelRoute.test.js
+++ src/routes/FullChannelRoute.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { render, screen } from '@folio/jest-config-stripes/testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@folio/jest-config-stripes/testing-library/react';
 import { Paneset } from '@folio/stripes/components';
 import withIntlConfiguration from '../../test/jest/util/withIntlConfiguration';
-import FullChannelRoute from './FullChannelRoute';
+import FullChannelRoute, { FullChannelRoute as RawFullChannelRoute } from './FullChannelRoute';
 import fullChannelData from '../../test/jest/data/fullChannel';
 import transformationData from '../../test/jest/data/transformation';
 
+
+const deleteChannel = jest.fn(() => {
+  console.log('*** deleting channel');
+});
 
 const renderFullChannelRoute = (channelResource) => {
   return render(withIntlConfiguration(
@@ -29,7 +33,7 @@ const renderFullChannelRoute = (channelResource) => {
               update: () => undefined,
             },
             channel: {
-              DELETE: () => undefined,
+              DELETE: deleteChannel,
             },
           }}
           match={{
@@ -73,5 +77,44 @@ describe('Full channel route', () => {
       const kvRoot = screen.getByText(`ui-inventory-import.channels.field.${name}`).closest('.kvRoot');
       expect(kvRoot.querySelector('[data-test-kv-value]')).toHaveTextContent(value);
     }
+
+    // transformationPipeline manifest returns correct path
+    const pathFn = RawFullChannelRoute.manifest.transformationPipeline.path;
+    const path = pathFn(undefined, undefined, undefined, undefined, {
+      resources: {
+        channel: {
+          records: [
+            {
+              transformationId: '12345',
+            },
+          ],
+        },
+      }
+    });
+    expect(path).toBe('inventory-import/transformations/12345');
+
+    // Pull down Actions menu and delete the current channel
+    const absentConfirmButton = screen.queryByText('ui-inventory-import.op.delete.confirm')
+    expect(absentConfirmButton).not.toBeInTheDocument();
+
+    const actionsMenu = container.querySelector('[data-pane-header-actions-dropdown]');
+    expect(actionsMenu).toBeVisible();
+    fireEvent.click(actionsMenu);
+
+    const deleteButton = container.querySelector('[data-test-delete-channel-button]');
+    expect(deleteButton).toBeInTheDocument();
+    await fireEvent.click(deleteButton);
+
+    const confirmButton = screen.queryByText('ui-inventory-import.op.delete.confirm')
+    expect(confirmButton).toBeInTheDocument();
+    await fireEvent.click(confirmButton);
+
+    // This doesn't properly check what we want it to, due to complexities of async code
+    setTimeout(async () => {
+      console.log('*** timer is done');
+      await waitFor(() => {
+        expect(deleteChannel).toHaveBeenCalledWith();
+      });
+    }, 1000);
   });
 });
