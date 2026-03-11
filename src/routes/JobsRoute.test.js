3409373fe7cf461eee83a7c56cb1c9e5c841d29b
Mike Taylor <mike@indexdata.com>
2026-03-11 16:01:50 +0000

    Add tests for jobs-route query function

--- src/routes/JobsRoute.test.js
+++ src/routes/JobsRoute.test.js
@@ -4,7 +4,7 @@ import { render, screen, fireEvent, waitFor } from '@folio/jest-config-stripes/t
 import userEvent from '@testing-library/user-event'
 import { Paneset } from '@folio/stripes/components';
 import withIntlConfiguration from '../../test/jest/util/withIntlConfiguration';
-import JobsRoute from './JobsRoute';
+import JobsRoute, { JobsRoute as RawJobsRoute } from './JobsRoute';
 import jobsData from '../../test/jest/data/jobs';
 
 
@@ -89,4 +89,48 @@ describe('Jobs route', () => {
     expect(screen.getByText('Error provoked by test harness')).toBeVisible();
     expect(screen.queryByText("Felix's test channel #1")).not.toBeInTheDocument();
   });
+});
+
+
+describe('should resolve query parameters to query correctly', () => {
+  const paramsFn = RawJobsRoute.manifest.jobs.params.query;
+  expect(paramsFn).toBeDefined();
+
+  const testCases = [
+    {
+      name: 'nothing included',
+      queryParameters: { qindex: 'channelName' },
+      pathComponents: {},
+      expected: undefined,
+    }, {
+      name: 'query provided but recordId omitted',
+      queryParameters: { qindex: 'channelName', query: 'samples' },
+      pathComponents: {},
+      expected: 'channelName="samples"',
+    }, {
+      name: 'query and recordId provided',
+      queryParameters: { qindex: 'channelName', query: 'samples' },
+      pathComponents: { recId: '67890' },
+      expected: 'channelId==67890 and channelName="samples"',
+    }, {
+      name: 'filters provided',
+      queryParameters: { filters: 'records_from.20,status.DONE,status.INTERRUPTED' },
+      pathComponents: {},
+      expected: 'amountImported>=20 and status==("DONE" or "INTERRUPTED")',
+    }, {
+      name: 'date-range specified',
+      queryParameters: { filters: 'started_from.2026-03-01,started_to.2026-03-11' },
+      pathComponents: {},
+      expected: 'started>=2026-03-01T00:00:00 and started<=2026-03-11T23:59:59',
+    }
+  ];
+  const logger = { log: () => undefined };
+
+  for (const { name, queryParameters, pathComponents, expected } of testCases) {
+    const resourceValues = { query: queryParameters };
+    it(`should make query for ${name}`, () => {
+      const res = paramsFn(queryParameters, pathComponents, resourceValues, logger);
+      expect(res).toBe(expected);
+    });
+  }
 });
