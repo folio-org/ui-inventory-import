14f5406f4e2837f0270b535821feb91d644f9eeb
Mike Taylor <mike@indexdata.com>
2026-03-17 09:07:41 +0000

    Remove more commented-out code

--- test/jest/setupFiles.js
+++ test/jest/setupFiles.js
@@ -1,14 +1,5 @@
 import './__mock__';
 
-/*
-import { TextDecoder, TextEncoder } from 'util';
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-*/
-
-// jest.mock('../../src/util/downloadCSV', () => jest.fn());
-
 // The version of Node used in CI seems to lack Intl.DurationFormat
 global.Intl.DurationFormat = class {
   format() {
