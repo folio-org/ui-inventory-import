bc2bac42559be703205f9e69e02dce080a04d366
Mike Taylor <mike@indexdata.com>
2026-03-18 17:17:47 +0000

    Import RTL functions from Stripes wrapper

--- src/settings/index.test.js
+++ src/settings/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render } from '@folio/jest-config-stripes/testing-library/react';
 import InventoryImportSettings from './index';
 
 // Mock react-intl
