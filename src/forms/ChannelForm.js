00d8826ab04931f797a2d2ca307143705620469a
Mike Taylor <mike@indexdata.com>
2026-09-07 16:21:45 +0100

    Show <NoValue /> in disable textbox when queuedFiles < 0

--- src/forms/ChannelForm.js
+++ src/forms/ChannelForm.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, useIntl } from 'react-intl';
 import arrayMutators from 'final-form-arrays';
-import { Row, Checkbox, Select, HasCommand, Button, LoadingPane, Pane, PaneFooter, checkScope } from '@folio/stripes/components';
+import { Row, Checkbox, Select, HasCommand, Button, LoadingPane, NoValue, Pane, PaneFooter, checkScope } from '@folio/stripes/components';
 import { AppIcon, TitleManager } from '@folio/stripes/core';
 import stripesFinalForm from '@folio/stripes/final-form';
 import { isEqual } from 'lodash';
 import setFieldData from 'final-form-set-field-data'; // XXX do we need this?
 import { RCF, CF } from '../components/CF';
+import { CKV } from '../components/CKV';
 
 
 const handleKeyCommand = (handler, { disabled } = {}) => {
@@ -125,7 +126,9 @@ const ChannelForm = (props) => {
             <br />
             <RCF tag="transformationId" i18nTag="transformationPipeline" component={Select} dataOptions={[noValue].concat(transformationPipelines)} required />
             <Row>
-              <CF tag="queuedFiles" xs={6} disabled />
+              {values.queuedFiles < 0 ?
+                <CKV rec={values} tag="queuedFiles" xs={6} formatFn={() => <NoValue />} /> :
+                <CF tag="queuedFiles" xs={6} disabled />}
               <CF tag="fileInProcess" xs={6} disabled />
             </Row>
           </form>
